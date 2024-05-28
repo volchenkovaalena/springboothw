b8f468bb2c662a881c2af8ebe98a68bdebd12943
alena <misamistake@>
2024-05-28 19:23:17 +0300

    Initial commit

---
+++ src/main/java/com/volchenkova/springboothw/SpringboothwApplication.java
@@ -0,0 +1,13 @@
+package com.volchenkova.springboothw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringboothwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringboothwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/volchenkova/springboothw/SpringboothwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.volchenkova.springboothw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringboothwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
